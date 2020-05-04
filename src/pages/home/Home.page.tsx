fb8988a4b7f713113b5dea47df082b2460cdafc5
Diego Chavez <diegchav.me@gmail.com>
2020-05-04 17:52:47 -0500

    Update posts state after deleting a post by filtering the delete post instead of getting all the posts again from the backend

--- src/pages/home/Home.page.tsx
+++ src/pages/home/Home.page.tsx
@@ -66,7 +66,7 @@ const HomePage = () => {
         try {
             await PostService.delete(postToDelete)
             setPostToDelete('')
-            const allPosts = await PostService.getAll()
+            const allPosts = posts.filter((post: any) => post._id !== postToDelete)
             setPosts(allPosts)
             setIsDeleteModalOpen(false)
         } catch (err) {
