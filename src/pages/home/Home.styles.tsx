bf4c30479d2c0c353842b83582ec6844d047ddef
Diego Chavez <diegchav.me@gmail.com>
2020-05-09 12:58:06 -0500

    Set width and height of home page to 100% to properly render loading spinner

--- src/pages/home/Home.styles.tsx
+++ src/pages/home/Home.styles.tsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
 
 const HomePageContainer = styled.div`
+    width: 100%;
+    height: 100%;
     display: flex;
     flex-direction: column;
     padding: 1rem 0;
