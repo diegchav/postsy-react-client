71f2658faad06ac8888071f1b84b7fd9684b87bc
Diego Chavez <diegchav.me@gmail.com>
2020-05-11 13:58:30 -0500

    Use postOwner field to render the name and the avatar of the feed post item

--- src/components/feed-post-item/FeedPostItem.component.tsx
+++ src/components/feed-post-item/FeedPostItem.component.tsx
@@ -6,16 +6,16 @@ interface FeedPostItemProps {
     item: any
 }
 
-const FeedPostItem = ({ item: { post } }: FeedPostItemProps) => {
+const FeedPostItem = ({ item: { post, postOwner } }: FeedPostItemProps) => {
     const imageNameIndex = post.imageUrl.split('/') + 1
     const imageName = post.imageUrl.substr(imageNameIndex)
 
     return (
         <FeedPostItemContainer>
             <div className="user-details">
-                <img className="avatar" src={post.user.avatar} alt="avatar" />
+                <img className="avatar" src={postOwner.avatar} alt="avatar" />
                 <div className="user">
-                    <p className="author">{post.user.name}</p>
+                    <p className="author">{postOwner.name}</p>
                     <p className="timestamp">{post.fromNow}</p>
                 </div>
             </div>
